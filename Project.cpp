001999dec71adec5ada0193bb9636fc8dd259de7
IjatAyam <ijat191999@gmail.com>
2018-11-27 23:55:49 +0800

    modified skit

--- Project.cpp
+++ Project.cpp
@@ -1,79 +1,71 @@
-#include<iostream>
+#include <iostream>
 #include <stdlib.h>
-#include<string.h>                  //contains strcmp(),strcpy(),strlen(),etc
-#include<ctype.h>                   //contains toupper(), tolower(),etc
-#include<dos.h>                     //contains _dos_getdate
-#include<time.h>
+#include <string.h>                  //contains strcmp(),strcpy(),strlen(),etc
+#include <time.h>
 #define RETURNTIME 15
 using namespace std;
+
 void sign_up(void);
 void login(int [], string []);
-int main()
-{
+
+int main(){
 	char choice;
-	
-	cout<<"\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 welcome to Ayam Bookstores \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2"<<endl;
-	
-	cout<<"already member ? (Y/N)"<<endl;
-	
-	cin>>choice;
-	for( ;choice!='Y'||choice!='N';)
-	{
-		if (choice=='Y')
-		{	
-			login();
-			break;
-		}
-		else if(choice =='N')
-		{
-			sign_up();
-			break;	
-		}
-		else;
-		{
-		cout<<"wrong input"<<endl;
-		cout<<"already member ? (Y/N)"<<endl;
-		cin>>choice;
-		}
-	}
-	
- return 0;	
-}
-void sign_up(void)
-{
-	
-	string name, pass;
+	string name[20], pass[20];
 	int z, s;
 	int Id_num[]={};
 	string npass[]={};
+
+	cout<<"\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 Welcome to Ayam Bookstores \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2"<<endl;
+
+	cout<<"Already member ? (Y/N)"<<endl;
+
+	cin>>choice;
+	for( ;choice!='Y'||choice!='N';){
+		if (choice=='Y'){
+			login(name, pass);
+			break;
+		}
+		else if(choice =='N'){
+			sign_up();
+			break;
+		}
+		else{
+		cout<<"Wrong input!!"<<endl;
+		cout<<"Already member ? (Y/N)"<<endl;
+		cin>>choice;
+		}
+	}
+
+ return 0;
+}
+void sign_up(string name[], string pass[]){
+
+
 	cout<<"PLEASE SIGN UP FIRST BEFORE ENTERING THE BOOKSTRORE!!!"<<endl;
 	cout<<"\xDB\xDB\xDB\xDB\xB2 1.ENTER NAME : ";
 	cin>>name;
+	cin.ignore();
 	cout<<"\xDB\xDB\xDB\xDB\xB2 2.ENTER ID NUM : ";
-	cin>>Id_num[];
+	cin>>Id_num;
 	cout<<"\xDB\xDB\xDB\xDB\xB2 3.Enter the password : ";
 	cin>>pass;
 	cin.ignore();
 	cout<<"\xDB\xDB\xDB\xDB\xB2 4.Retype your password : ";
-	cin>>npass[];
+	cin>>npass;
+	cin.ignore();
 	login(z,s);
-	
+
 	}
-void login(int num[], string X[])
-{ 
+void login(int num[], string X[]){
 	string pass;
 	int id_num;
-	
+
 	cout<<"Enter your Id : ";
 	cin>>id_num;
 	cout<<"Enter your password : ";
 	cin>>pass;
-	
-	if(id_num==num&& pass==X)
-	{
+
+	if(id_num==num&& pass==X){
 		cout<<"T";
 	}
 }
-
-
-
